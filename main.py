4b752566b94ef96e6ab09876af69cd3cdab2e04b
OlgaBeznutrova <olga.beznutrova@gmail.com>
2022-03-28 18:58:06 +1200

    set webhook instead of long polling connection

--- main.py
+++ main.py
@@ -164,18 +164,24 @@ server = Flask(__name__)
 
 
 @server.route("/" + token, methods=["POST"])
-def get_messages():
-    bot.process_new_updates([types.Update.de_json(request.stream.read().decode("utf-8"))])
+def get_message():
+    json_string = request.get_data().decode("utf-8")
+    update = types.Update.de_json(json_string)
+    bot.process_new_updates([update])
     return "!", 200
 
 
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url=os.getenv("HEROKU_URL") + token)
-    return "!", 200
+    bot.set_webhook(url=os.getenv("BOT_URL") + token)
+    return "!!", 200
 
 
 # application entry point
 if __name__ == '__main__':
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8443)))
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+# if __name__ == '__main__':
+#    bot.remove_webhook()
+#    bot.infinity_polling()
